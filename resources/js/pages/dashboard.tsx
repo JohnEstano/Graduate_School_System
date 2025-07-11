6002875222a438ec05195b67d461ee29568fd7b1
JohnEstano <jestano_230000001603@uic.edu.ph>
2025-07-12 01:43:57 +0800

    Added routes under web.php, Added React pages for each links and linked their corresponding routes

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -29,28 +29,32 @@ export default function Dashboard() {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
 
-            <div className="flex pl-7 flex-col gap-1 p-4">
-                <h1 className="text-3xl font-bold">
-                    {user?.name ?? 'Guest'}
-                </h1>
-                <p className="text-sm text-gray-400">
-                    Student/ Dean/ AA / Coordinator
-                </p>
-            </div>
 
 
 
-            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl pl-7 pr-7">
+            <div className="flex h-full flex-1 flex-col pt-5 gap-4 rounded-xl pl-7 pr-7 overflow-auto">
+                <div className="flex flex-col gap-1">
+                    <h1 className="text-3xl font-bold">
+                        {user?.name ?? 'Guest'}
+                    </h1>
+                    <p className="text-sm text-gray-400">
+                        Student/ Dean/ AA / Coordinator
+                    </p>
+                </div>
+
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+
                     </div>
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+
                     </div>
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+
                     </div>
+                </div>
+                <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
+                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                 </div>
                 <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
                     <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
---
+++ resources/js/pages/payment/Index.tsx
@@ -0,0 +1,43 @@
+import AppLayout from '@/layouts/app-layout';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/react';
+import { usePage } from '@inertiajs/react';
+
+const breadcrumbs: BreadcrumbItem[] = [
+
+    {
+        title: 'Dashboard',
+        href: '/dashboard',
+    },
+    {
+        title: 'Payments',
+        href: '/payment',
+    },
+];
+
+
+
+export default function Index() {
+
+
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Payments" />
+
+
+
+
+            <div className="flex h-full flex-1 flex-col pt-5 gap-4 rounded-xl pl-7 pr-7 overflow-auto">
+
+
+                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
+
+                    </div>
+
+                </div>
+
+            </div>
+        </AppLayout>
+    );
+}
