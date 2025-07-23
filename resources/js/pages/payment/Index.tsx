5f5fddf61c474b2b53dc2dcd8a5c906465a6e6ff
JohnEstano <jestano_230000001603@uic.edu.ph>
2025-07-23 16:45:27 +0800

    stuff

--- resources/js/pages/payment/Index.tsx
+++ resources/js/pages/payment/Index.tsx
@@ -28,7 +28,7 @@ export default function Index() {
 
 
             <div className="flex h-full flex-1 flex-col pt-5 gap-4 rounded-xl pl-7 pr-7 overflow-auto">
-
+<h1>hello</h1>
 
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
